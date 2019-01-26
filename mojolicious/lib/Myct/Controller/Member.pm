d69b46b1db6d08e7bebfc2b29c53037da6562891
kashiwagi <kashiwagi@kashiwaginoMacBook-Air.local>
2019-01-27 00:20:26 +0900

    add user regist function

--- mojolicious/lib/Myct/Controller/Member.pm
+++ mojolicious/lib/Myct/Controller/Member.pm
@@ -1,27 +1,20 @@
 package Myct::Controller::Member;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojo::File;
+use Mojo::JSON qw(encode_json decode_json);
 
 sub info {
     my $self = shift;
 
-    my $users = [
-        {
-            id   => 1,
-            name => 'user1',
-            role => '警察',
-            team => '警察',
-            live => 1,
-        },
-        {
-            id   => 2,
-            name => 'user1',
-            role => '泥棒',
-            team => '泥棒',
-            live => 1,
-        },
-    ];
+    my $path   = Mojo::File->new('local.json');
+    my $handle = $path->open('<');
+    my $bytes  = $path->slurp;
+    my $data   = decode_json $bytes;
 
-    $self->render(template => 'member/info', users => $users );    
+    my $users  = $data->{users};
+    my $msg    = $self->flash('msg') or '';
+
+    $self->render(template => 'member/info', users => $users, msg => $msg );
 }
 
 sub regist {
@@ -31,7 +24,34 @@ sub regist {
 
 sub regist_action {
     my $self = shift;
-    $self->render(template => 'member/info');
+
+    my $name = $self->param('name');
+    my $role = $self->param('role');
+
+    my $path   = Mojo::File->new('local.json');
+    my $handle = $path->open('>>');
+    my $bytes  = $path->slurp;
+    my $data   = decode_json $bytes;
+
+    my $users       = $data->{users};
+    my @users_array = @{$users};
+    my $id          = @users_array;
+    $id+=1;
+
+    push(@users_array, {
+       id   => $id,
+       name => $name,
+       role => $role,
+       live => 1,
+    });
+
+    $data->{users} = [@users_array];
+
+    my $json = encode_json $data;
+    $path = $path->spurt($json);
+
+    $self->flash(msg => "ユーザを追加しました");
+    $self->redirect_to('/member/info');
 }
 
 1;
