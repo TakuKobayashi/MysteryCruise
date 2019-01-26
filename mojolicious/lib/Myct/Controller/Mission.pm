3c460a4616c034f13f493c977e493742b2374767
kashiwagi <kashiwagi@kashiwaginoMacBook-Air.local>
2019-01-26 21:40:30 +0900

    create data.json

--- mojolicious/lib/Myct/Controller/Mission.pm
+++ mojolicious/lib/Myct/Controller/Mission.pm
@@ -1,14 +1,17 @@
 package Myct::Controller::Mission;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojo::File;
+use Mojo::JSON qw(encode_json decode_json);
 
 sub info {
     my $self = shift;
+    my $msg  = $self->flash('msg') or '';
 
     my $missions = [
         {
             id   => 1,
             text => '明石海峡大橋を通過しました。',
-            time => '12:50',
+            time => '11:50',
             is_publish => '1',
         },
         {
@@ -19,13 +22,21 @@ sub info {
         },
     ];
 
-    $self->render(template => 'mission/info', missions => $missions );    
+    $self->render(template => 'mission/info', missions => $missions, msg => $msg );    
 }
 
 sub publish {
     my $self = shift;
 
-    $self->render(template => 'mission/info', msg => "司令を配信しました");
+    my $mission = $self->param('mission');
+
+    my $path   = Mojo::File->new('../data.json');
+    my $handle = $path->open('>>');
+    my $bytes  = $path->slurp;
+    my $data   = decode_json $bytes;
+
+    $self->flash(msg => "司令を配信しました $bytes");
+    $self->redirect_to('/mission/info');
 }
 
 1;
